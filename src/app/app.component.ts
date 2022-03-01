3d0e6ca6104361e0ec366f38ebea523cb0e2332a
Caique Reimberg <caiquecreimberg@gmail.com>
2022-03-01 11:36:47 -0300

    feature: implementado form inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'divisores-front';
+  primes = [1, 2];
+  numberSelected: any;
+
+  divisors = [1, 2, 5, 6];
+
+  public calculate(): void {
+    if (!this.numberSelected || this.numberSelected < 1) {
+      Swal.fire('Necessário digitar um número que seja positivo e maior que zero!');
+    }
+    console.log(this.numberSelected);
+  }
 }
